bd62af9992d116f5d42d74623973ea439cd0edf6
ren trigoso <renzotrigoso@gmail.com>
2019-08-20 19:21:13 -0400

    added anon function example

--- concurrency/main.go
+++ concurrency/main.go
@@ -4,16 +4,31 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"time"
 )
 
 func main() {
-	go echo(os.Stdin, os.Stdout)
-	time.Sleep(30 * time.Second)
-	fmt.Println("Timed out.")
+	callEchoExample()
+	callAnonFunction()
 	os.Exit(0)
 }
 
 func echo(in io.Reader, out io.Writer) {
 	io.Copy(out, in)
+}
+
+func callEchoExample() {
+	go echo(os.Stdin, os.Stdout)
+	time.Sleep(10 * time.Second)
+	fmt.Println("[callEchoExample] Timed out.")
+}
+
+func callAnonFunction() {
+	fmt.Println("[callAnonFunction] outside goroutine")
+	go func() {
+		fmt.Println("[callAnonFunction] inside a goroutine")
+	}()
+	fmt.Println("[callAnonFunction] outside goroutine again")
+	runtime.Gosched()
 }
