88b9bb0c2f994c47d7bb0f0e0a9fcd0e022e10bb
ren trigoso <rtrigoso@cbcnewmedia.com>
2019-08-20 22:04:28 -0400

    added callMutexLockExample

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
 	"runtime"
+	"strings"
+	"sync"
 	"time"
 )
 
 func main() {
-	callEchoExample()
+	// callEchoExample()
 	callAnonExample()
+	callMutexLockExample()
 	os.Exit(0)
 }
 
@@ -31,4 +35,71 @@ func callAnonExample() {
 	}()
 	fmt.Println("[callAnonExample] outside goroutine again")
 	runtime.Gosched()
+}
+
+type words struct {
+	sync.Mutex
+	found map[string]int
+}
+
+func newWords() *words {
+	return &words{found: map[string]int{}}
+}
+
+func (w *words) add(word string, n int) {
+	w.Lock()
+	defer w.Unlock()
+	count, ok := w.found[word]
+	if !ok {
+		w.found[word] = n
+		return
+	}
+
+	w.found[word] = count + n
+}
+
+func tallyWords(filename string, dict *words) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		words := strings.ToLower(scanner.Text())
+		dict.add(words, 1)
+	}
+
+	return scanner.Err()
+}
+
+func callMutexLockExample() {
+	fmt.Println("[callMutexLockExample] start")
+	var wg sync.WaitGroup
+	files := []string{"./data/tallytest.txt", "./data/tallytest1.txt"}
+
+	w := newWords()
+	for _, f := range files {
+		wg.Add(1)
+		go func(file string) {
+			fmt.Println("[callMutexLockExample] testing file ", file)
+			if err := tallyWords(file, w); err != nil {
+				fmt.Printf("[callMutexLockExample] error: %s", err.Error())
+				os.Exit(1)
+			}
+			wg.Done()
+		}(f)
+	}
+	wg.Wait()
+
+	fmt.Println("[callMutexLockExample] Words that appear more than once:")
+
+	for word, count := range w.found {
+		if count > 1 {
+			fmt.Println(word, " - ", count)
+		}
+	}
 }
